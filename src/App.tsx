9d6014be4a26eaf417153535e1476093d57e3727
Dezolutions <mars92851@gmail.com>
2021-08-03 16:03:13 +0300

    added starter template

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+  React.useEffect(() => {
+    const fetchPosts = async () => {
+      const data = await axios.get('https://graph.instagram.com/{user-id}?fields=id,username&access_token={access-token}')
+    }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/components/Post.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Post = () => {
+  return (
+    <div>
+      <img src="" alt="" />
+      <div>
+        <div className="likes"></div>
+        <div className="post-text"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Post
